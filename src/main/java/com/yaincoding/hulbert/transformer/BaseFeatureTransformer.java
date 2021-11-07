9b702a027b888bca417734e2b434d8d427833275
yaincoding <yaincoding@gmail.com>
2021-11-07 22:53:14 +0900

    feature transformer for base features

---
+++ src/main/java/com/yaincoding/hulbert/transformer/BaseFeatureTransformer.java
@@ -0,0 +1,24 @@
+package com.yaincoding.hulbert.transformer;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.yaincoding.hulbert.pos.Pos;
+
+public class BaseFeatureTransformer extends FeatureTransformer {
+
+	@Override
+	protected List<String> toFeature(List<String> words, List<Pos> tags, int i) {
+		List<String> features = new ArrayList<>();
+
+		features.add(String.format("x[0]=%s", words.get(i)));
+		features.add(String.format("x[0]=%s, y[-1]=%s", words.get(i), tags.get(i - 1)));
+		features.add(String.format("x[-1:0]=%s-%s", words.get(i - 1), words.get(i)));
+		features.add(
+				String.format("x[-1:0]=%s-%s, y[-1]=%s", words.get(i - 1), words.get(i), tags.get(i - 1)));
+		features.add(String.format("x[-1,1]=%s-%s", words.get(i - 1), words.get(i + 1)));
+		features.add(String.format("x[-1,1]=%s-%s, y[-1]=%s", words.get(i - 1), words.get(i + 1),
+				tags.get(i - 1)));
+
+		return features;
+	}
+}

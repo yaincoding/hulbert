f12622354f6a9d985cbc5bc4923c0df5402bc7cd
정창걸 <changgeol@zuminternet.com>
2021-11-08 18:01:48 +0900

    UNK tag for unknown words added

--- src/main/java/com/yaincoding/hulbert/pos/Pos.java
+++ src/main/java/com/yaincoding/hulbert/pos/Pos.java
@@ -3,5 +3,5 @@ package com.yaincoding.hulbert.pos;
 public enum Pos {
 
     NNP, NNG, NP, NNB, MAG, NR, SW, SE, SL, SH, SF, SP, SO, SN, XR, XSN, XPN, XSV, XSA, EF, EC, MM, VV, VA, VCP, VX,
-    JKC, JKG, JKS, JKO, JKB, JX, IC, ETM, SS, NA, BOS, EOS;
+    JKC, JKG, JKS, JKO, JKB, JX, IC, ETM, SS, NA, BOS, EOS, UNK;
 }

bb5b353030f6011a8f3657ef93f09fc609ca8f9c
정창걸 <changgeol@zuminternet.com>
2021-11-09 18:00:52 +0900

    pos tagger with trigram model

--- src/main/java/com/yaincoding/hulbert/tag/TrigramTagger.java
+++ src/main/java/com/yaincoding/hulbert/tag/TrigramTagger.java
@@ -1,19 +1,38 @@
 package com.yaincoding.hulbert.tag;
 
+import java.io.IOException;
 import java.util.List;
-import com.yaincoding.hulbert.representation.WordPos;
-import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
+import com.yaincoding.hulbert.model.Beam;
+import com.yaincoding.hulbert.model.Model;
+import com.yaincoding.hulbert.model.TrigramModel;
+import com.yaincoding.hulbert.representation.Eojeol;
+import com.yaincoding.hulbert.representation.Eojeols;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
-@RequiredArgsConstructor
 public class TrigramTagger implements Tagger {
 
-    @Override
-    public List<WordPos> tag(String sentence, boolean flatten, boolean debug) {
-        return null;
+    private final Model model;
+
+    public TrigramTagger() throws IOException {
+        String modelPath = System.getenv("TRIGRAM_MODEL_PATH");
+        model = new TrigramModel(modelPath);
     }
 
-    
+    @Override
+    public List<Eojeols> tag(String sentence, boolean flatten, boolean debug) {
+
+        List<List<Eojeol>> beginIndex = model.lookup(sentence, false);
+        String chars = StringUtils.trimAllWhitespace(sentence);
+
+        Beam beam = Beam.builder().topK(5).unknownPenalty(model.UNKNOWN_PENALTY).aSyllablePenalty(-0.3)
+                .nounPreference(0.4).longerNounPreference(0.3).model(model).build();
+
+        List<Eojeols> topEojeols = beam.search(beginIndex, chars);
+        return topEojeols.subList(1, topEojeols.size() - 1);
+    }
+
 }

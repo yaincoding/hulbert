b278ca02076034eef24b59bdff260db8bc66938d
정창걸 <changgeol@zuminternet.com>
2021-11-09 17:59:33 +0900

    some tags added

--- src/main/java/com/yaincoding/hulbert/pos/Pos.java
+++ src/main/java/com/yaincoding/hulbert/pos/Pos.java
@@ -2,6 +2,6 @@ package com.yaincoding.hulbert.pos;
 
 public enum Pos {
 
-    NNP, NNG, NP, NNB, MAG, NR, SW, SE, SL, SH, SF, SP, SO, SN, XR, XSN, XPN, XSV, XSA, EF, EC, MM, VV, VA, VCP, VX,
-    JKC, JKG, JKS, JKO, JKB, JX, IC, ETM, SS, NA, BOS, EOS, UNK;
+    NNP, NNG, NP, NNB, MAG, MAJ, NR, SW, SE, SL, SH, SF, SP, SO, SN, XR, XSN, XPN, XSV, XSA, EF, EC, EP, ETN, MM, VV, VCN,
+    VA, VCP, VX, JKC, JKG, JKS, JKO, JKB, JKV, JKQ, JC, JX, IC, ETM, SS, NA, BOS, EOS, UNK;
 }

5038ce54a49fd7885066759269ab49aff41af327
yaincoding <yaincoding@gmail.com>
2021-11-18 11:31:51 +0900

    nodel params use default value when they are not given

--- src/main/java/com/yaincoding/hulbert/controller/Controller.java
+++ src/main/java/com/yaincoding/hulbert/controller/Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.yaincoding.hulbert.representation.Eojeols;
 import com.yaincoding.hulbert.tag.Tagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,19 +21,43 @@ public class Controller {
 
     private final Tagger tagger;
 
+    @Value("${modelparam.a_syllable_penalty}")
+    private double A_SYLLABLE_PENALTY;
+
+    @Value("${modelparam.noun_preference}")
+    private double NOUN_PREFERENCE;
+
+    @Value("${modelparam.longer_noun_preference}")
+    private double LONGER_NOUN_PREFERENCE;
+
+    @Value("${modelparam.unknown_penalty}")
+    private double UNKNOWN_PENALTY;
+
     @GetMapping
     public ResponseEntity<List<Eojeols>> analyze(String s,
             @RequestParam(name = "a_syllable_penalty", required = false) Double aSyllablePenalty,
             @RequestParam(name = "noun_preference", required = false) Double nounPreference,
             @RequestParam(name = "longer_noun_preference", required = false) Double longerNounPreference,
             @RequestParam(name = "unknown_penalty", required = false) Double unknownPenalty) {
-        if (aSyllablePenalty != null || nounPreference != null || longerNounPreference != null
-                || unknownPenalty != null) {
-            return ResponseEntity
-                    .ok(tagger.tag(s, true, aSyllablePenalty, nounPreference, longerNounPreference, unknownPenalty));
+
+        if (aSyllablePenalty == null) {
+            aSyllablePenalty = A_SYLLABLE_PENALTY;
         }
 
-        return ResponseEntity.ok(tagger.tag(s, true));
+        if (nounPreference == null) {
+            nounPreference = NOUN_PREFERENCE;
+        }
+
+        if (longerNounPreference == null) {
+            longerNounPreference = LONGER_NOUN_PREFERENCE;
+        }
+
+        if (unknownPenalty == null) {
+            unknownPenalty = UNKNOWN_PENALTY;
+        }
+
+        return ResponseEntity
+                .ok(tagger.tag(s, true, aSyllablePenalty, nounPreference, longerNounPreference, unknownPenalty));
     }
 
 }

9a3781d040978c803694203c050b635d06d427f9
yaincoding <yaincoding@gmail.com>
2021-11-18 11:04:24 +0900

    model params is changed with outer config params

--- src/main/java/com/yaincoding/hulbert/tag/TrigramTagger.java
+++ src/main/java/com/yaincoding/hulbert/tag/TrigramTagger.java
@@ -9,6 +9,7 @@ import com.yaincoding.hulbert.model.TrigramModel;
 import com.yaincoding.hulbert.representation.Eojeol;
 import com.yaincoding.hulbert.representation.Eojeols;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -17,18 +18,41 @@ public class TrigramTagger implements Tagger {
 
     private final Model model;
 
+    @Value("${modelparam.a_syllable_penalty}")
+    private double A_SYLLABLE_PENALTY;
+
+    @Value("${modelparam.noun_preference}")
+    private double NOUN_PREFERENCE;
+
+    @Value("${modelparam.longer_noun_preference}")
+    private double LONGER_NOUN_PREFERENCE;
+
+    @Value("${modelparam.unknown_penalty}")
+    private double UNKNOWN_PENALTY;
+
     public TrigramTagger() throws IOException {
         String modelPath = System.getenv("TRIGRAM_MODEL_PATH");
         model = new TrigramModel(modelPath);
     }
 
     @Override
-    public List<Eojeols> tag(String sentence, boolean flatten, boolean debug) {
-
+    public List<Eojeols> tag(String sentence, boolean flatten) {
         List<List<Eojeol>> beginIndex = model.lookup(sentence, false);
 
-        Beam beam = Beam.builder().topK(5).unknownPenalty(model.UNKNOWN_PENALTY).aSyllablePenalty(-0.3)
-                .nounPreference(0.4).longerNounPreference(0.3).model(model).build();
+        Beam beam = Beam.builder().topK(5).unknownPenalty(UNKNOWN_PENALTY).aSyllablePenalty(A_SYLLABLE_PENALTY)
+                .nounPreference(NOUN_PREFERENCE).longerNounPreference(LONGER_NOUN_PREFERENCE).model(model).build();
+
+        List<Eojeols> topEojeols = beam.search(beginIndex, StringUtils.trimAllWhitespace(sentence));
+        return topEojeols;
+    }
+
+    @Override
+    public List<Eojeols> tag(String sentence, boolean flatten, double aSyllablePenalty, double nounPreference,
+            double longerNounPreference, double unknownPenalty) {
+        List<List<Eojeol>> beginIndex = model.lookup(sentence, false);
+
+        Beam beam = Beam.builder().topK(5).unknownPenalty(unknownPenalty).aSyllablePenalty(aSyllablePenalty)
+                .nounPreference(nounPreference).longerNounPreference(longerNounPreference).model(model).build();
 
         List<Eojeols> topEojeols = beam.search(beginIndex, StringUtils.trimAllWhitespace(sentence));
         return topEojeols;

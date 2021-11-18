c599699ef6430f26803f465dcdd417cd925fad19
yaincoding <yaincoding@gmail.com>
2021-11-18 11:01:55 +0900

    added request params for parameter tuning

--- src/main/java/com/yaincoding/hulbert/controller/Controller.java
+++ src/main/java/com/yaincoding/hulbert/controller/Controller.java
@@ -8,6 +8,7 @@ import com.yaincoding.hulbert.tag.Tagger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,18 @@ public class Controller {
     private final Tagger tagger;
 
     @GetMapping
-    public ResponseEntity<List<Eojeols>> analyze(String s) {
-        return ResponseEntity.ok(tagger.tag(s, true, false));
+    public ResponseEntity<List<Eojeols>> analyze(String s,
+            @RequestParam(name = "a_syllable_penalty", required = false) Double aSyllablePenalty,
+            @RequestParam(name = "noun_preference", required = false) Double nounPreference,
+            @RequestParam(name = "longer_noun_preference", required = false) Double longerNounPreference,
+            @RequestParam(name = "unknown_penalty", required = false) Double unknownPenalty) {
+        if (aSyllablePenalty != null || nounPreference != null || longerNounPreference != null
+                || unknownPenalty != null) {
+            return ResponseEntity
+                    .ok(tagger.tag(s, true, aSyllablePenalty, nounPreference, longerNounPreference, unknownPenalty));
+        }
+
+        return ResponseEntity.ok(tagger.tag(s, true));
     }
+
 }
